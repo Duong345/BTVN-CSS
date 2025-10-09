6e705ea65018091d0e61c1aee7109c25a11ab89b
Hoàng Thái Dương_20225821 <thaiduong040404@gmail.com>
2025-10-10 01:40:09 +0700

    update btvn

--- Practise/main.js
+++ Practise/main.js
@@ -1,5 +1,18 @@
-var girl = document.querySelector(".child--girl");
-var p = girl.nextElementSibling;
-var input = p.nextElementSibling;
-console.log(p);
-console.log(input);
+const group = document.querySelector(".button-group");
+const addBtn = document.querySelector(".add-btn");
+const logBtn = document.querySelector(".log-btn");
+const input = document.querySelector('input[name="phone-number"]');
+addBtn.addEventListener("click", () => {
+  const p = document.createElement("p");
+  const now = new Date().toLocaleDateString();
+  p.textContent = `Hello world + ${now}`;
+  group.insertBefore(p, addBtn.nextElementSibling);
+});
+logBtn.addEventListener("click", () => {
+  const phoneNumber = input.value;
+  if (phoneNumber === "") {
+    alert("phone number undefined");
+  } else {
+    console.log("phone number:", phoneNumber);
+  }
+});
