c0170208d73090191816ff8494bdaedda7653a3b
Hoàng Thái Dương_20225821 <thaiduong040404@gmail.com>
2025-10-17 09:09:35 +0700

    update

--- main.js
+++ main.js
@@ -5,21 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
   addBtn.addEventListener("click", () => {
     const currentCount = inputList.querySelectorAll("input").length;
     const newIndex = currentCount + 1;
-
     const newInput = document.createElement("input");
     newInput.type = "text";
     newInput.name = "phone-number";
     newInput.placeholder = `${newIndex - 1} `;
-
     inputList.appendChild(newInput);
   });
 
   logBtn.addEventListener("click", () => {
     const inputs = inputList.querySelectorAll("input[name='phone-number']");
-    console.clear();
-    console.log("All typed phone numbers:");
     inputs.forEach((input, index) => {
-      console.log(`{ ${index + 1} }; { ${input.value} }`);
+      console.log(`{ ${index} }; { ${input.value} }`);
     });
   });
 });
